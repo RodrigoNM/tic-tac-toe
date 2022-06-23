1444acc0fe2220b19ccacd98677aef081eac559d
rodrigo <iam_rnmagalhaes@idcontractors.net>
2022-06-23 20:55:00 -0300

    implemets socket

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
 // view engine setup
-app.use(express.static('views'))
+app.use(express.static(path.join(__dirname , '/views')));
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
